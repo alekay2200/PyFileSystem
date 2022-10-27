fe709b9c2eae25b6b4de7366a22f0f5b5ed291ef
alekay2200 <alejandropalomino22@gmail.com>
2022-10-27 17:56:23 +0200

    Added correct classifiers

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     packages=["fs"],
 
     classifiers=[
-        "Programmin Language :: Python :: 3",
-        "License :: GNU General Public License v3.0",
-        "Operative System :: OS Independent" 
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent" 
     ],
 )
